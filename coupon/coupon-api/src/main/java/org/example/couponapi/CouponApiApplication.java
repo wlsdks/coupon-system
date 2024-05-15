9bf8f009e50f73de30db44b6c354dedef707e03f
wlsdks <wlsdks12@naver.com>
2024-05-15 18:43:53 +0900

    :memo: 모듈 yaml 적용관련 주석 추가

--- coupon/coupon-api/src/main/java/org/example/couponapi/CouponApiApplication.java
+++ coupon/coupon-api/src/main/java/org/example/couponapi/CouponApiApplication.java
@@ -10,6 +10,11 @@ import org.springframework.context.annotation.Import;
 public class CouponApiApplication {
 
     public static void main(String[] args) {
+
+        /**
+         * 명시적으로 application-core.yml과 application-api.yml 설정을 불러와 합친다는 의미다.
+         * 이렇게 설정하면, application-core.yml에 있는 데이터베이스 설정과 같은 항목들이 CouponApiApplication에서도 사용될 수 있다.
+         */
         System.setProperty("spring.config.name", "application-core, application-api");
         SpringApplication.run(CouponApiApplication.class, args);
     }
