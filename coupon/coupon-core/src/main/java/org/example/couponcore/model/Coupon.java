48446856c2ee0a25af99d4bbb920737bb378cd82
wlsdks <wlsdks12@naver.com>
2024-05-16 00:14:35 +0900

    feat: 쿠폰 발급에 관련된 도메인 로직 추가

--- coupon/coupon-core/src/main/java/org/example/couponcore/model/Coupon.java
+++ coupon/coupon-core/src/main/java/org/example/couponcore/model/Coupon.java
@@ -44,4 +44,31 @@ public class Coupon extends BaseTimeEntity {
     @Column(nullable = false)
     private LocalDateTime dateIssueEnd;
 
+    // 발급 가능한 수량이 남아있는지 확인한다.
+    public boolean availableIssueQuantity() {
+        // 발급 수량에 제한이 없는 경우
+        if (totalQuantity == null) {
+            return true;
+        }
+        return totalQuantity > issueQuantity;
+    }
+
+    // 현재 시간이 쿠폰 발급 시작 날짜 이후이면서 동시에 발급 종료 날짜 이전인 경우에만 true를 반환해.
+    public boolean availableIssueDate() {
+        LocalDateTime now = LocalDateTime.now();
+        // 쿠폰 발급 시작기간이 지금 이전인가? && 쿠폰 발급 종료기간이 지금 이후인가?
+        return dateIssueStart.isBefore(now) && dateIssueEnd.isAfter(now);
+    }
+
+    // 발급된 수량을 증가시킨다.
+    public void issue() {
+        if (!availableIssueQuantity()) {
+            throw new RuntimeException("쿠폰 발급 가능 수량을 초과했습니다.");
+        }
+        if (!availableIssueDate()) {
+            throw new RuntimeException("쿠폰 발급 기간이 아닙니다.");
+        }
+        issueQuantity++;
+    }
+
 }
