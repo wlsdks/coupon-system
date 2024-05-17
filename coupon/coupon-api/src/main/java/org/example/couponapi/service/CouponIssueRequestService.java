397a4870443f87c5700a85330dd6e0e371b8cdbc
wlsdks <wlsdks12@naver.com>
2024-05-18 00:33:36 +0900

    feat: redisson을 사용한 redis 분산락 코드 구현 및 서비스 로직에서 적용
    기존의 synchronized 로직을 redis 분산락 로직으로 변경

--- coupon/coupon-api/src/main/java/org/example/couponapi/service/CouponIssueRequestService.java
+++ coupon/coupon-api/src/main/java/org/example/couponapi/service/CouponIssueRequestService.java
@@ -3,6 +3,7 @@ package org.example.couponapi.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.couponapi.controller.dto.CouponIssueRequestDto;
+import org.example.couponcore.component.DistributeLockExecutor;
 import org.example.couponcore.service.CouponIssueService;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,13 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor distributeLockExecutor;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        synchronized (this) {
+        // 분산락 처리
+        distributeLockExecutor.execute("lock_" + requestDto.couponId(), 10000, 10000, () -> {
             couponIssueService.issue(requestDto.couponId(), requestDto.userId());
-        }
+        });
         log.info("쿠폰 발급 완료. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
     }
 
