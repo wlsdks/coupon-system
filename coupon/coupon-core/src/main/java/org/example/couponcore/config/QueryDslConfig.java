2c1094c32387111a3a4ca5cdc0b4f97d78254967
wlsdks <wlsdks12@naver.com>
2024-05-17 19:55:37 +0900

    feat: querydsl 설정 코드 작성
    gradle에서 coupon-core > other > compileJava를 실행하면 QClass가 만들어진다.

---
+++ coupon/coupon-core/src/main/java/org/example/couponcore/config/QueryDslConfig.java
@@ -0,0 +1,22 @@
+package org.example.couponcore.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
