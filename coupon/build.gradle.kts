104d36e2b89ae648d146f16607811763e4ee545b
wlsdks <wlsdks12@naver.com>
2024-05-17 19:53:16 +0900

    feat: querydsl 의존성 추가

--- coupon/build.gradle.kts
+++ coupon/build.gradle.kts
@@ -39,6 +39,12 @@ subprojects {
         implementation("org.springframework.boot:spring-boot-starter-data-jpa")
         implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
+        // querydsl
+        implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+        annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+        annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+        annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
         // lombok
         compileOnly("org.projectlombok:lombok")
         annotationProcessor("org.projectlombok:lombok")
