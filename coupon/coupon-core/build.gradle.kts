3db768cae546a3b04b5ba4a3138ba4df9afe5fb8
wlsdks <wlsdks12@naver.com>
2024-05-18 00:04:27 +0900

    feat: 레디스 분산락 구현을 위해 redisson 의존성 추가

--- coupon/coupon-core/build.gradle.kts
+++ coupon/coupon-core/build.gradle.kts
@@ -8,6 +8,8 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    // redis의 분산락을 구현하기 위해 redisson을 사용
+    implementation("org.redisson:redisson-spring-boot-starter:3.30.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
