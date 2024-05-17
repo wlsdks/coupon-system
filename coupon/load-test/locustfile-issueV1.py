96cdda741790e56537c14cc8f44819096e6788fb
wlsdks <wlsdks12@naver.com>
2024-05-17 22:40:20 +0900

    feat: 로커스트 테스트 조건 수정

---
+++ coupon/load-test/locustfile-issueV1.py
@@ -0,0 +1,17 @@
+import random
+from locust import task, FastHttpUser
+
+class CouponIssueV1(FastHttpUser):
+    connection_timeout = 10.0
+    network_timeout = 10.0
+
+    # 부하 테스트를 위한 task 정의
+    @task
+    def issue(self):
+        payload = {
+            # "userId": 1, # 중복 요청 발급에 대한 테스트 목적으로 userId를 1로 고정
+            "userId": random.randint(1, 100000000),
+            "couponId": 1 # 이미 테이블에 1이 저장되어있다고 가정
+        }
+        with self.rest("POST", "/v1/issue", json=payload):
+            pass
