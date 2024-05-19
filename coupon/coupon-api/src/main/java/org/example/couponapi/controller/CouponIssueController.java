aecc198750d71d9a5c9b69654ec317fce13350ec
wlsdks <wlsdks12@naver.com>
2024-05-19 20:36:02 +0900

    feat: api 컨트롤러 로직 수정 (레디스 스크립트를 호출하는 api)

--- coupon/coupon-api/src/main/java/org/example/couponapi/controller/CouponIssueController.java
+++ coupon/coupon-api/src/main/java/org/example/couponapi/controller/CouponIssueController.java
@@ -14,17 +14,36 @@ public class CouponIssueController {
 
     private final CouponIssueRequestService couponIssueRequestService;
 
-    // 쿠폰 발급 요청
+    /**
+     * 쿠폰 발급 요청
+     * @param requestDto
+     * @return
+     */
     @PostMapping("/v1/issue")
     public CouponIssueResponseDto issueV1(@RequestBody CouponIssueRequestDto requestDto) {
         couponIssueRequestService.issueRequestV1(requestDto);
         return new CouponIssueResponseDto(true, null);
     }
 
-    // 쿠폰 발급 요청
+    /**
+     * 비동기로 쿠폰 발급 요청 v1
+     * @param requestDto
+     * @return
+     */
     @PostMapping("/v1/issue-async")
     public CouponIssueResponseDto asyncIssueV1(@RequestBody CouponIssueRequestDto requestDto) {
         couponIssueRequestService.asyncIssueRequestV1(requestDto);
+        return new CouponIssueResponseDto(true, null);
+    }
+
+    /**
+     * 비동기로 쿠폰 발급 요청 v2
+     * @param requestDto
+     * @return
+     */
+    @PostMapping("/v2/issue-async")
+    public CouponIssueResponseDto asyncIssueV2(@RequestBody CouponIssueRequestDto requestDto) {
+        couponIssueRequestService.asyncIssueRequestV2(requestDto);
         return new CouponIssueResponseDto(true, null);
     }
 
--- coupon/coupon-api/src/main/java/org/example/couponapi/service/CouponIssueRequestService.java
+++ coupon/coupon-api/src/main/java/org/example/couponapi/service/CouponIssueRequestService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.couponapi.controller.dto.CouponIssueRequestDto;
 import org.example.couponcore.component.DistributeLockExecutor;
 import org.example.couponcore.service.AsyncCouponIssueServiceV1;
+import org.example.couponcore.service.AsyncCouponIssueServiceV2;
 import org.example.couponcore.service.CouponIssueService;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ public class CouponIssueRequestService {
     private final CouponIssueService couponIssueService;
     private final AsyncCouponIssueServiceV1 asyncCouponIssueServiceV1;
     private final DistributeLockExecutor distributeLockExecutor;
+    private final AsyncCouponIssueServiceV2 asyncCouponIssueServiceV2;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
         // 분산락 처리
@@ -26,11 +28,19 @@ public class CouponIssueRequestService {
     }
 
     /**
-     * 비동기로 쿠폰 발급 요청
+     * 비동기로 쿠폰 발급 요청 v1 (redis)
      * @param requestDto
      */
     public void asyncIssueRequestV1(CouponIssueRequestDto requestDto) {
         asyncCouponIssueServiceV1.issue(requestDto.couponId(), requestDto.userId());
+    }
+
+    /**
+     * 비동기로 쿠폰 발급 요청 v2 (redis script)
+     * @param requestDto
+     */
+    public void asyncIssueRequestV2(CouponIssueRequestDto requestDto) {
+        asyncCouponIssueServiceV2.issue(requestDto.couponId(), requestDto.userId());
     }
 
 }
