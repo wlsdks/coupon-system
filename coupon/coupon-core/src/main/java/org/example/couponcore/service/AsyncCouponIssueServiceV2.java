3fbc7344aa4fa8fb98a7682af81ccf7fcbc8a1e1
wlsdks <wlsdks12@naver.com>
2024-05-19 23:02:59 +0900

    chore: 사용하지 않는 주석 제거

--- coupon/coupon-core/src/main/java/org/example/couponcore/service/AsyncCouponIssueServiceV2.java
+++ coupon/coupon-core/src/main/java/org/example/couponcore/service/AsyncCouponIssueServiceV2.java
@@ -19,8 +19,6 @@ public class AsyncCouponIssueServiceV2 {
      * @param userId
      */
     public void issue(long couponId, long userId) {
-        // 로컬 캐시를 사용하면 좀 더 빠르게 처리할 수 있다.
-//        CouponRedisEntity coupon = couponCacheService.getCouponCache(couponId);
         CouponRedisEntity coupon = couponCacheService.getCouponLocalCache(couponId);
         coupon.checkIssuableCoupon();
         issueRequest(couponId, userId, coupon.totalQuantity());
