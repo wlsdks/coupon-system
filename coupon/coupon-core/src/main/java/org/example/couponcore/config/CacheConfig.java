d405185479f40825e002e59c4ccd0008ad85e9f8
wlsdks <wlsdks12@naver.com>
2024-05-19 19:17:13 +0900

    feat: 레디스 캐싱 설정을 위한 코드 작성

---
+++ coupon/coupon-core/src/main/java/org/example/couponcore/config/CacheConfig.java
@@ -0,0 +1,37 @@
+package org.example.couponcore.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * 캐시 설정
+ */
+@RequiredArgsConstructor
+@Configuration
+public class CacheConfig {
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public CacheManager redisCacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(30)); // 캐시 만료 시간 30분
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+
+}
